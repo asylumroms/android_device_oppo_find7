4acab916ba33a80fed03b227f11ca598e869cbd8
chasmodo <bosko.trkulja@gmail.com>
2015-11-03 21:56:23 +0100

    Duify

--- du.mk
+++ du.mk
@@ -22,26 +22,26 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
-# Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Enhanced NFC
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# Inherit from the common product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# must be before including omni part
+# Boobanimation
 TARGET_BOOTANIMATION_SIZE := 720x480
 
 # no DSPManager for us
 TARGET_NO_DSPMANAGER := true
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/oppo/find7/device.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := omni_find7
+PRODUCT_NAME := du_find7
 PRODUCT_DEVICE := find7
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := FIND7
@@ -50,8 +50,8 @@ PRODUCT_MANUFACTURER := OPPO
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_PRODUCT=FIND7 \
     TARGET_DEVICE=FIND7 \
-    BUILD_FINGERPRINT="4.3/JLS36C/1390465867:user/release-keys" \
-    PRIVATE_BUILD_DESC="msm8974-user 4.3 JLS36C eng.root.20140510.152835 release-keys"
+    BUILD_FINGERPRINT=5.0/LRX21M/1426582430:user/release-keys \
+    PRIVATE_BUILD_DESC="msm8974-user 5.0 LRX21M 193 release-keys"
 
 # Inline kernel
 TARGET_KERNEL_SOURCE := kernel/oppo/msm8974
